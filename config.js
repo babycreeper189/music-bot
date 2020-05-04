c6d9df253645ef32d3695e15dfdbae72e7974c11
babycreeper189 <63827080+babycreeper189@users.noreply.github.com>
2020-05-04 09:23:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.token = "Njc4OTgzOTAyMjk0OTY2Mjg0.Xq2MxQ.yRROduOsYyamfj-5a7f4poFnNkY";
+exports.token = "your token here";
 exports.owner = ["640637667637329920", "475911595713757205"];
 exports.prefix = "c!";
